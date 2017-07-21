2a30551a5cd6e2ab74555943f00eeb67f558db42
Matthew Curtis <matthew.curtis@uncorkedstudios.com>
2017-07-20 17:01:03 -0700

    Cube things

--- Hexagon.js
+++ Hexagon.js
@@ -8,10 +8,10 @@ const hexagonPrototypeGenerator = function (sideLength) {
 
   const draw = function (canvasContext, mouseX, mouseY) {
     let distance = Math.sqrt(Math.pow(this.centerX - mouseX, 2) + Math.pow(this.centerY - mouseY, 2));
+
     if (distance < this.radius * 10) {
       canvasContext.fillStyle = this.color;
       canvasContext.strokeStyle = this.color;
-      let distance = Math.sqrt(Math.pow(this.centerX - mouseX, 2) + Math.pow(this.centerY - mouseY, 2));
 
       canvasContext.beginPath();
       canvasContext.moveTo(this.vector1[0], this.vector1[1]);
@@ -22,17 +22,23 @@ const hexagonPrototypeGenerator = function (sideLength) {
       canvasContext.lineTo(this.vector6[0], this.vector6[1]);
       canvasContext.closePath();
 
-      canvasContext.globalAlpha = Math.max(((1 / (10 * this.radius)) * -1) * distance + 1, 0);
-
       if (mouseX && mouseY && canvasContext.isPointInPath(mouseX, mouseY)) {
+        canvasContext.globalAlpha = 0.1;
+        drawWall(canvasContext, this.vectorC, this.vector2, this.vector1, this.vector6); // Top
         canvasContext.globalAlpha = 1.0;
+        drawWall(canvasContext, this.vectorC, this.vector2, this.vector3, this.vector4); // Front right
+        canvasContext.globalAlpha = 0.5;
+        drawWall(canvasContext, this.vectorC, this.vector4, this.vector5, this.vector6); // Front left
+        // drawWall(canvasContext, this.vectorC, this.vector3, this.vector4, this.vector5); // Bottom
+        // canvasContext.globalAlpha = 0.4;
+        // drawWall(canvasContext, this.vectorC, this.vector5, this.vector6, this.vector1); // Back left
+        // drawWall(canvasContext, this.vectorC, this.vector1, this.vector2, this.vector3); // Back right
       } else {
         let distanceOpacity = ((1 / (10 * this.radius)) * -1) * distance + 1;
         canvasContext.globalAlpha = Math.max(distanceOpacity - this.randomness, 0);
+        // canvasContext.fill();
+        // canvasContext.stroke();
       }
-
-      canvasContext.fill();
-      canvasContext.stroke();
     }
   }
 
@@ -46,6 +52,17 @@ const hexagonPrototypeGenerator = function (sideLength) {
   }
 }
 
+function drawWall(canvasContext, vector1, vector2, vector3, vector4) {
+  canvasContext.beginPath();
+  canvasContext.moveTo(vector1[0], vector1[1]);
+  canvasContext.lineTo(vector2[0], vector2[1]);
+  canvasContext.lineTo(vector3[0], vector3[1]);
+  canvasContext.lineTo(vector4[0], vector4[1]);
+  canvasContext.closePath();
+  canvasContext.fill();
+  canvasContext.stroke();
+}
+
 // Variables specific to individual hexagons
 const Hexagon = function (x, y, hex) {
   let color = "maroon";
@@ -57,12 +74,13 @@ const Hexagon = function (x, y, hex) {
     randomness: Math.random(),
     centerX: x + hex.rectangleWidth / 2,
     centerY: y + hex.sideLength,
-    vector1: [x + hex.radius, y],
-    vector2: [x + hex.rectangleWidth, y + hex.height],
-    vector3: [x + hex.rectangleWidth, y + hex.height + hex.sideLength],
-    vector4: [x + hex.radius, y + hex.rectangleHeight],
-    vector5: [x, y + hex.sideLength + hex.height],
-    vector6: [x, y + hex.height]
+    vectorC: [x + hex.rectangleWidth / 2, y + hex.sideLength],
+    vector1: [x + hex.radius, y], // Top
+    vector2: [x + hex.rectangleWidth, y + hex.height], // Top right
+    vector3: [x + hex.rectangleWidth, y + hex.height + hex.sideLength], // Bottom right
+    vector4: [x + hex.radius, y + hex.rectangleHeight], // Bottom
+    vector5: [x, y + hex.sideLength + hex.height], // Bottom left
+    vector6: [x, y + hex.height] // Top left
   }
 
   return Object.setPrototypeOf(newHex, hex);
